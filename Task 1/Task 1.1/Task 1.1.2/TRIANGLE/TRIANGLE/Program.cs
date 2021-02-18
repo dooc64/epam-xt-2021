ed4397c0c0c28d27f9d78e104ccb7f31594ae547
Maksim Kulebyakin <vfrcbvrek64@mail.ru>
2021-02-18 12:46:27 +0400

    feat: added TRIANGLE task

---
+++ Task 1/Task 1.1/Task 1.1.2/TRIANGLE/TRIANGLE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TRIANGLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Введите кол-во строк: ");
+                bool correct = int.TryParse(Console.ReadLine(), out int height);
+                if (!correct)
+                {
+                    Console.WriteLine("ОШИБКА ВВОДА!");
+                    continue;
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                for (int i = 0; i < height; i++)
+                {
+                    for (int y = 0; y <= i; y++)
+                    {
+                        Console.Write('*');
+                    }
+                    Console.WriteLine();
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
