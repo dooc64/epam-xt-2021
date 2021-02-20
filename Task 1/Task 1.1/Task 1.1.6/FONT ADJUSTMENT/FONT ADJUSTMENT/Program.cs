bb4ad8494325021fcf09381efc55170db70cb4a7
Maksim Kulebyakin <vfrcbvrek64@mail.ru>
2021-02-20 23:34:44 +0400

    feat: added FONT ADJUSTMENT task

---
+++ Task 1/Task 1.1/Task 1.1.6/FONT ADJUSTMENT/FONT ADJUSTMENT/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FONT_ADJUSTMENT
+{
+    class Program
+    {
+        private static bool bold = false;
+        private static bool italic = false;
+        private static bool underline = false;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ShowSettings();
+                bool correct = int.TryParse(Console.ReadLine(), out int input);
+                if(!correct && input < 4 && input > 0)
+                {
+                    Console.WriteLine("Выберите цифру 1, 2 или 3");
+                    continue;
+                }
+                SwitchFont(input);
+            }
+        }
+
+        static void SwitchFont(int input)
+        {
+            switch(input)
+            {
+                case 1:
+                    bold = !bold;
+                    break;
+                case 2:
+                    italic = !italic;
+                    break;
+                case 3:
+                    underline = !underline;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void ShowSettings()
+        {
+            Console.Write("Параметры надписи: ");
+            Console.Write(!bold && !italic && !underline ? "None" : "");
+            Console.Write(bold ? "Bold " : "");
+            Console.Write(italic ? "Italic " : "");
+            Console.Write(underline ? "Underline " : "");
+            Console.WriteLine();
+            Console.WriteLine("Введите:");
+            Console.WriteLine("       1: bold");
+            Console.WriteLine("       2: italic");
+            Console.WriteLine("       3: underline");
+        }
+    }
+}
