3508c84a42f856e43e609171d9c83d81769c7881
Maksim Kulebyakin <vfrcbvrek64@mail.ru>
2021-02-24 22:58:40 +0400

    feat: added ARRAY PROCESSING task

---
+++ Task 1/Task 1.1/Task 1.1.7/ARRAY PROCESSING/ARRAY PROCESSING/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ARRAY_PROCESSING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] mass = new int[10];
+
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                mass[i] = rnd.Next(0, 10);
+            }
+
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+
+            BubbleSort(mass);
+            Console.WriteLine();
+            for (int i = 0; i < mass.Length - 1; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void BubbleSort(int[] mass)
+        {
+            int temp;
+
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                if(mass[i] > mass[i + 1])
+                {
+                    temp = mass[i + 1];
+
+                    mass[i + 1] = mass[i];
+                    mass[i] = temp;
+
+                    i = 0;
+                }
+            }
+        }
+    }
+}
