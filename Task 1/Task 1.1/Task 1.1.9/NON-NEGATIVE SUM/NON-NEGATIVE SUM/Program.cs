6b6149cc6d446919deaa60acb3ae8207bd9db86a
Maksim Kulebyakin <vfrcbvrek64@mail.ru>
2021-03-01 19:17:00 +0400

    feat: added NON-NEGATIVE SUM task

---
+++ Task 1/Task 1.1/Task 1.1.9/NON-NEGATIVE SUM/NON-NEGATIVE SUM/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NON_NEGATIVE_SUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int[10];
+            Randomaize(mass);
+            Show(mass);
+            Console.WriteLine("====");
+            Console.WriteLine("Сумма положительных элементов массива: " + PositiveSum(mass));
+            Console.ReadLine();
+        }
+
+        static void Randomaize(int[] mass)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                mass[i] = rnd.Next(-10, 10);
+            }
+        }
+
+        static int PositiveSum(int[] mass)
+        {
+            int sum = 0;
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                if (mass[i] > 0)
+                    sum += mass[i];
+            }
+            return sum;
+        }
+
+        static void Show(int[] mass)
+        {
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+        }
+    }
+}
