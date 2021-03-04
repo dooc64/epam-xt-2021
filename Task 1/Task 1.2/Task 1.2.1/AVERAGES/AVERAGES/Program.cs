cccc84602c38082b4769f5041e5da5f20a07faa8
Maksim Kulebyakin <vfrcbvrek64@mail.ru>
2021-03-04 14:35:44 +0400

    feat: added AVERAGES task

---
+++ Task 1/Task 1.2/Task 1.2.1/AVERAGES/AVERAGES/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace AVERAGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку...");
+            string text = Console.ReadLine();
+            string[] ClearText = ClearWords(text);
+            Console.WriteLine("Средняя длина слов составила: " + AvgLengthWords(ClearText));
+            Console.ReadLine();
+        }
+
+        static string[] ClearWords(string words)
+        {
+            char[] tresh = new char[] { ' ', ';', ',', ':', '.', '!', '?', '-' };
+            string[] inputData = words.Split(tresh, StringSplitOptions.RemoveEmptyEntries);
+            return inputData;
+        }
+        
+        static int AvgLengthWords(string[] words)
+        {
+            int sum = 0;
+
+            foreach (var item in words)
+            {
+                sum += item.Length;
+            }
+
+            //Rounding a number
+            int average = sum / words.Length;
+
+            return average;
+        }
+    }
+}
