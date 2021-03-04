7ea0e378ce86a5afefc5096acc68bd8a795acb3a
Maksim Kulebyakin <vfrcbvrek64@mail.ru>
2021-03-04 16:58:36 +0400

    feat: added LOWERCASE task

---
+++ Task 1/Task 1.2/Task 1.2.3/LOWERCASE/LOWERCASE/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace LOWERCASE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш текст...");
+            string input = Console.ReadLine();
+            string[] cleanresult = TextCleaner(input);
+            int count = LowerCaseCounter(cleanresult);
+            Console.WriteLine($"Кол-во слов с маленькой буквы: {count}");
+            Console.ReadLine();
+        }
+
+        static string[] TextCleaner(string input)
+        {
+            char[] separator = new char[] { ' ', ',', '.', ':', ';', '!', '?' };
+            return input.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        static int LowerCaseCounter(string[] text)
+        {
+            int count = 0;
+            foreach (var item in text)
+            {
+                if (char.IsLower(item[0]))
+                    count++;
+            }
+            return count;
+        }
+
+        static int LowerCaseCounterLinq(string[] input)
+        {
+            return input.Where(x => char.IsLower(x[0])).Count();
+        }
+    }
+}
