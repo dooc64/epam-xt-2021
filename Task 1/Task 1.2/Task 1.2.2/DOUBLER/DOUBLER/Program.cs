ce2f2e587f5d013e8c3c494435d52c7be9c0d49d
Maksim Kulebyakin <vfrcbvrek64@mail.ru>
2021-03-04 16:20:06 +0400

    feat: added DOUBLER task

---
+++ Task 1/Task 1.2/Task 1.2.2/DOUBLER/DOUBLER/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOUBLER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую строку...");
+            string input = Console.ReadLine();
+            Console.WriteLine("А теперь вторую...");
+            string inputSecond = Console.ReadLine();
+            Console.WriteLine(Doubler(input, inputSecond));
+            Console.ReadLine();
+        }
+
+        static string Doubler(string firstInput, string secondInput)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < firstInput.Length; i++)
+            {
+                if (firstInput[i] == ' ')
+                    break;
+
+                result.Append(firstInput[i]);
+
+                if (secondInput.Contains(firstInput[i]))
+                    result.Append(firstInput[i]);
+            }
+            return result.ToString();
+        }
+    }
+}
